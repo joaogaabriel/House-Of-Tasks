26e2b3c5d3c31c0ee9fb91d2e9f61a01270ca981
joaogaabriel <joaogabriel13ferreira@gmail.com>
2025-02-20 19:45:08 -0300

    .

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -7,20 +7,19 @@ export const createTask = async (req: Request, res: Response) => {
 
     if (!title || !description) {
       res.status(400).json({ message: "Título e descrição são obrigatórios!" });
-      return; // Aqui, como estamos em uma condição de erro, podemos retornar para evitar que a função continue
+      return;
     }
 
-    // Criação da tarefa sem o campo 'id', pois ele é gerado automaticamente
     const newTask = await prisma.task.create({
       data: {
         title,
         description,
-        status: "PENDING", // ou o valor que você desejar
-        userId, // O ID do usuário que será associado à tarefa
+        status: "PENDING",
+        userId,
       },
     });
 
-    res.status(201).json(newTask); // Responde com a nova tarefa criada
+    res.status(201).json(newTask);
   } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : "Erro desconhecido";
