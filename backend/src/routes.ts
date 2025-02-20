78de33956edbafcb51d926c7ae93291c7494370f
IgorArthur <igorart1210@gmail.com>
2025-02-20 00:04:49 -0300

    Creates updateUser and deleteUser

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,18 +3,23 @@ import {
   createUser,
   getUsers,
   getUserById,
+  updateUser,
+  deleteUser
 } from "./controllers/userController";
 import { createTask, getTasksByUser } from "./controllers/taskController";
 import { login } from "./controllers/authController";
 
 const router = Router();
 
-router.post("/users", createUser);
+router.get("/users", getUsers);
 router.get("/users/:id", getUserById);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
-router.get("/userss", getUsers);
 router.post("/tasks", createTask);
 router.get("/tasks/:userId", getTasksByUser);
+
 router.post("/login", login);
 
 export default router;
